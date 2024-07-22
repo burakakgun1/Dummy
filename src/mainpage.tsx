70e82aeeefeb4a3764c116d8cb6c5a9520af6b89
burakakgun1 <akgun1087@gmail.com>
2024-07-22 17:17:11 +0300

    iconlar eklendi

--- src/mainpage.tsx
+++ src/mainpage.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeadphones,faUtensils } from "@fortawesome/free-solid-svg-icons";
 
 const MainPage: React.FC = () => {
   return (
@@ -10,6 +12,7 @@ const MainPage: React.FC = () => {
           <Link to="/products" className="text-decoration-none">
             <Card className="h-100 shadow-sm">
               <Card.Body className="d-flex flex-column align-items-center">
+                <FontAwesomeIcon icon={faHeadphones} size="3x" className="mb-3" />
                 <Card.Title className="mb-3">Products</Card.Title>
                 <Card.Text>Click to view our product list</Card.Text>
               </Card.Body>
@@ -20,6 +23,7 @@ const MainPage: React.FC = () => {
           <Link to="/recipes" className="text-decoration-none">
             <Card className="h-100 shadow-sm">
               <Card.Body className="d-flex flex-column align-items-center">
+              <FontAwesomeIcon icon={faUtensils} size="3x" className="mb-3" />
                 <Card.Title className="mb-3">Recipes</Card.Title>
                 <Card.Text>Click to explore our recipes</Card.Text>
               </Card.Body>
